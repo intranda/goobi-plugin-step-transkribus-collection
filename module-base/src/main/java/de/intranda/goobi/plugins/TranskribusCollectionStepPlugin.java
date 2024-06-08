309c66d8c19183a9c4d15e7b9da8f24b43980a74
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2024-06-08 11:30:34 +0200

    initial commit

--- module-base/src/main/java/de/intranda/goobi/plugins/TranskribusCollectionStepPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/TranskribusCollectionStepPlugin.java
@@ -36,10 +36,10 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
-public class KickStartStepPlugin implements IStepPluginVersion2 {
+public class TranskribusCollectionStepPlugin implements IStepPluginVersion2 {
     
     @Getter
-    private String title = "intranda_step_kick_start";
+    private String title = "intranda_step_transkribus_collection";
     @Getter
     private Step step;
     @Getter
@@ -57,7 +57,7 @@ public class KickStartStepPlugin implements IStepPluginVersion2 {
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
         value = myconfig.getString("value", "default value"); 
         allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
-        log.info("KickStart step plugin initialized");
+        log.info("TranskribusCollection step plugin initialized");
     }
 
     @Override
@@ -70,7 +70,7 @@ public class KickStartStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public String getPagePath() {
-        return "/uii/plugin_step_kick_start.xhtml";
+        return "/uii/plugin_step_transkribus_collection.xhtml";
     }
 
     @Override
@@ -109,7 +109,7 @@ public class KickStartStepPlugin implements IStepPluginVersion2 {
         boolean successful = true;
         // your logic goes here
         
-        log.info("KickStart step plugin executed");
+        log.info("TranskribusCollection step plugin executed");
         if (!successful) {
             return PluginReturnValue.ERROR;
         }
